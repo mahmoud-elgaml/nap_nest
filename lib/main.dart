bc3b7880858287ed597685f7470ac1b4a0c8fae8
Mahmoud Tarek Fouad <melgamel@std.mans.edu.eg>
2025-05-13 16:32:56 +0300

    Implement PSQI result view with pie chart and progress indicators

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nap_nest/core/helper/on_generate_route.dart';
 import 'package:nap_nest/core/services/shared_preferences_singleton.dart';
 import 'package:nap_nest/core/utils/app_colors.dart';
-import 'package:nap_nest/features/splash/presentation/view/splash_view1.dart';
+import 'package:nap_nest/features/psqi/presentation/view/psqi_result_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,7 +38,7 @@ class NapNest extends StatelessWidget {
         useMaterial3: true,
       ),
       onGenerateRoute: onGenerateRoute,
-      initialRoute: SplashView1.routeName,
+      initialRoute: PsqiResultView.routeName,
     );
   }
 }
