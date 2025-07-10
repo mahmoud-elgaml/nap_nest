0d0f7d5d25b5edb6e41570b2a1e0f82b7c179160
mahmoud tarek <melgamel@std.mans.edu.eg>
2025-07-10 05:09:23 +0300

    Adjust reverse transition duration and refine easing curve for iOS page transition

--- lib/core/helpers/custom_transition.dart
+++ lib/core/helpers/custom_transition.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class CustomIOSPageRoute<T> extends PageRouteBuilder<T> {
-  
   final Widget page;
+
   CustomIOSPageRoute({required this.page})
     : super(
         transitionDuration: const Duration(milliseconds: 450),
-        reverseTransitionDuration: const Duration(milliseconds: 350),
+        reverseTransitionDuration: const Duration(milliseconds: 300),
         pageBuilder: (context, animation, secondaryAnimation) => page,
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           final slide = Tween<Offset>(
             begin: const Offset(1.0, 0.0),
             end: Offset.zero,
-          ).chain(CurveTween(curve: Curves.easeOutQuart));
+          ).chain(CurveTween(curve: Curves.easeOutCubic));
 
           final fade = Tween<double>(
             begin: 0.0,
